c6103ea349c844c31c4ac740ddc13f58b8924c35
Hosein21378 <153272840+Hosein21378@users.noreply.github.com>
2026-07-28 20:13:38 +0330

    Update admin.py

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -7,6 +7,7 @@ async def admin_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
 
     admin_ids = [int(os.getenv("ADMIN_ID", "0"))]
+    
     if update.effective_user.id not in admin_ids:
         await query.edit_message_text("⛔ شما دسترسی به پنل مدیریت ندارید.")
         return
